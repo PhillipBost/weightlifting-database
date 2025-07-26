b12ece0f6d100d7dbc8ebfb7814ba14ce598d5c2
Phillip Bost <phillipbost@gmail.com>
2025-07-26 18:05:58 -0400

    Update meet_scraper_2025.js

--- meet_scraper_2025.js
+++ meet_scraper_2025.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
-const {handleTotalAthleteString, getAmountMeetsOnPage} = require('../../utils/string_utils');
+const {handleTotalAthleteString, getAmountMeetsOnPage} = require('./utils/string_utils');
 
 // Ensure directories exist
 function ensureDirectoryExists(dirPath) {
