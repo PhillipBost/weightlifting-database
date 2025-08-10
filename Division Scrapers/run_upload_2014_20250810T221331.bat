34592d1770be0a9556699ede498c52c85933fd7f
Phillip Bost <phillipbost@gmail.com>
2025-08-10 18:28:52 -0400

    Big file reorg

---
+++ Division Scrapers/run_upload_2014_20250810T221331.bat
@@ -0,0 +1,19 @@
+@echo off
+			echo ================================================
+			echo ATHLETE CSV UPLOADER - BACKGROUND PROCESS
+			echo ================================================
+			echo Starting upload at %date% %time%
+			echo.
+
+			node athlete-csv-uploader.js
+
+			echo.
+			echo ================================================
+			echo Upload completed at %date% %time%
+			echo Window will close in 3 seconds...
+			echo ================================================
+			timeout /t 3 /nobreak >nul
+			exit;
+			
+			REM Delete this batch file after upload
+			del "%~f0"
